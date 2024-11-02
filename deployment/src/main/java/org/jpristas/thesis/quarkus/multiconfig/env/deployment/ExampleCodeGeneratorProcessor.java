fbbe6cec210367177a0736bace37268700278992
Jakub Pristas <jakub.pristas@ibm.com>
2024-11-02 21:58:19 +0100

    implemented reading config properties from application.properties file for propdoc

--- deployment/src/main/java/org/jpristas/thesis/quarkus/multiconfig/env/deployment/ExampleCodeGeneratorProcessor.java
+++ deployment/src/main/java/org/jpristas/thesis/quarkus/multiconfig/env/deployment/ExampleCodeGeneratorProcessor.java
@@ -7,6 +7,7 @@ import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.Properties;
 
 import ca.mrvisser.propdoc.VelocityMain;
 import org.jboss.logging.Logger;
@@ -20,23 +21,53 @@ import ca.mrvisser.propdoc.impl.writer.VelocityPropDocWriterImpl;
 
 public class ExampleCodeGeneratorProcessor {
     private static final Logger LOG = Logger.getLogger(ExampleCodeGeneratorProcessor.class);
-    //private static final String FILE_NAME = ".env";
-    private static final String FILE_NAME = "commons.properties";
-
-    private static final String VELOCITY_TEMPLATE_URL = "classpath:templates/template.properties.vm";
-
-    //private static final String VELOCITY_TEMPLATE_URL = "classpath:templates/template.env.vm";
 
     @BuildStep
-    void generateFile(ConfigDataBuildItem configData, OutputTargetBuildItem outputTarget, BuildProducer<GeneratedResourceBuildItem> resourceProducer) throws IOException, URISyntaxException {
+    void generateFiles(ConfigDataBuildItem configData, OutputTargetBuildItem outputTarget, BuildProducer<GeneratedResourceBuildItem> resourceProducer) throws IOException {
         if (configData.getFileContent().isEmpty()) {
             LOG.warn("No configuration data available. Skipping file generation.");
             return;
         }
 
-        Path outputPath = Paths.get(outputTarget.getOutputDirectory().toString(), FILE_NAME);
+        Properties properties = new Properties();
+        properties.load(new StringReader(configData.getFileContent()));
 
-        // Create the file and ensure parent directories exist
+        boolean generateCmFile = Boolean.parseBoolean(properties.getProperty("quarkus.prop-doc.template_cm", "false"));
+        boolean generateEnvFile = Boolean.parseBoolean(properties.getProperty("quarkus.prop-doc.template_env", "false"));
+        boolean generatePropertiesFile = Boolean.parseBoolean(properties.getProperty("quarkus.prop-doc.template_properties", "false"));
+
+        String targetEnvironment = properties.getProperty("quarkus.prop-doc.target_environment", "dev");
+
+        String fileContent = configData.getFileContent();
+
+        if (generateCmFile) {
+            generateCmFile(fileContent, targetEnvironment, outputTarget, resourceProducer);
+        }
+        if (generateEnvFile) {
+            generateEnvFile(fileContent, targetEnvironment, outputTarget, resourceProducer);
+        }
+        if (generatePropertiesFile) {
+            generatePropertiesFile(fileContent, targetEnvironment, outputTarget, resourceProducer);
+        }
+    }
+
+    private void generateCmFile(String fileContent, String targetEnvironment, OutputTargetBuildItem outputTarget, BuildProducer<GeneratedResourceBuildItem> resourceProducer) throws IOException {
+        generateFile(fileContent, "classpath:templates/template.cm.vm", "config.cm", targetEnvironment, outputTarget, resourceProducer);
+    }
+
+    private void generateEnvFile(String fileContent, String targetEnvironment, OutputTargetBuildItem outputTarget, BuildProducer<GeneratedResourceBuildItem> resourceProducer) throws IOException {
+        generateFile(fileContent, "classpath:templates/template.env.vm", "config.env", targetEnvironment, outputTarget, resourceProducer);
+    }
+
+    private void generatePropertiesFile(String fileContent, String targetEnvironment, OutputTargetBuildItem outputTarget, BuildProducer<GeneratedResourceBuildItem> resourceProducer) throws IOException {
+        generateFile(fileContent, "classpath:templates/template.properties.vm", "config.properties", targetEnvironment, outputTarget, resourceProducer);
+    }
+
+    private void generateFile(String fileContent, String templatePath, String outputFileName, String targetEnvironment,
+                              OutputTargetBuildItem outputTarget, BuildProducer<GeneratedResourceBuildItem> resourceProducer) throws IOException {
+
+        Path outputPath = Paths.get(outputTarget.getOutputDirectory().toString(), outputFileName);
+
         File outputFile = outputPath.toFile();
         if (!outputFile.getParentFile().exists() && !outputFile.getParentFile().mkdirs()) {
             LOG.error("Failed to create directories for " + outputFile.getAbsolutePath());
@@ -45,20 +76,18 @@ public class ExampleCodeGeneratorProcessor {
 
         // Process the content and write it using PropDocWriter
         try (OutputStream out = new FileOutputStream(outputFile)) {
-            PropDoc propDoc = new PropDoc(new ByteArrayInputStream(configData.getFileContent().getBytes(StandardCharsets.UTF_8)));
-            PropDocWriter propDocWriter = new VelocityPropDocWriterImpl(VELOCITY_TEMPLATE_URL, "dev", false);
+            PropDoc propDoc = new PropDoc(new ByteArrayInputStream(fileContent.getBytes(StandardCharsets.UTF_8)));
+            PropDocWriter propDocWriter = new VelocityPropDocWriterImpl(templatePath, targetEnvironment, false);
             propDocWriter.write(propDoc, out);
             LOG.info("File generated and processed with PropDoc successfully: " + outputFile.getAbsolutePath());
         } catch (Exception e) {
-            LOG.error("Failed to generate file", e);
-            return;
+            LOG.error("Failed to generate file for template: " + templatePath, e);
         }
 
-        // Read the generated file content back into a byte array
-        byte[] fileContent = Files.readAllBytes(outputPath);
-
         // Produce the generated resource
-        resourceProducer.produce(new GeneratedResourceBuildItem(FILE_NAME, fileContent));
-        LOG.info("Resource registered successfully: " + FILE_NAME);
+        byte[] generatedFileContent = Files.readAllBytes(outputPath);
+        resourceProducer.produce(new GeneratedResourceBuildItem(outputFileName, generatedFileContent));
+        LOG.info("Resource registered successfully: " + outputFileName);
     }
+
 }
