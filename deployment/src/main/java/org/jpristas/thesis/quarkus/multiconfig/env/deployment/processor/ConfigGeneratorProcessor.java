c40066d2a002377a6b4a114e7c1ce1fb3f01d1c6
Jakub Pristas <jakub.pristas@ibm.com>
2025-02-06 23:20:22 +0100

    outputPathProperty

--- deployment/src/main/java/org/jpristas/thesis/quarkus/multiconfig/env/deployment/processor/ConfigGeneratorProcessor.java
+++ deployment/src/main/java/org/jpristas/thesis/quarkus/multiconfig/env/deployment/processor/ConfigGeneratorProcessor.java
@@ -46,7 +46,7 @@ public class ConfigGeneratorProcessor {
         if (propFileName == null || propFileName.trim().isEmpty()) {
             propFileName = "default.properties";
         }
-
+        // TODO
         //String outputPathProperty = properties.getProperty("quarkus.prop-doc.output-path", "").trim();
 
         String fileContent = configData.getFileContent();
