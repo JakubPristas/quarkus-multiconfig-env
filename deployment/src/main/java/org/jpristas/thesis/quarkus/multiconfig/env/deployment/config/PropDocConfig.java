68a5665ed4565b5dfca31ae97a00c0139688bbe0
Jakub Pristas <jakub.pristas@ibm.com>
2025-02-21 00:09:49 +0100

    added config file for Quarkus Config properties

---
+++ deployment/src/main/java/org/jpristas/thesis/quarkus/multiconfig/env/deployment/config/PropDocConfig.java
@@ -0,0 +1,66 @@
+package org.jpristas.thesis.quarkus.multiconfig.env.deployment.config;
+
+import java.io.StringReader;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Properties;
+
+public class PropDocConfig {
+
+    private final List<String> templatesToGenerate;
+    private final String targetEnvironment;
+    private final String cmFileName;
+    private final String envFileName;
+    private final String propFileName;
+    private final String outputPath;
+    private final boolean outputDescription;
+
+    public PropDocConfig(String fileContent) {
+        Properties properties = new Properties();
+        try {
+            properties.load(new StringReader(fileContent));
+        } catch (Exception e) {
+            throw new RuntimeException("Failed to load configuration properties", e);
+        }
+
+        this.templatesToGenerate = Arrays.asList(properties.getProperty("quarkus.prop-doc.selected-templates", "").split(","));
+        this.targetEnvironment = properties.getProperty("quarkus.prop-doc.target_environment", "dev");
+        this.cmFileName = getPropertyOrDefault(properties, "quarkus.prop-doc.cm.file-name", "default.cm");
+        this.envFileName = getPropertyOrDefault(properties, "quarkus.prop-doc.env.file-name", "default.env");
+        this.propFileName = getPropertyOrDefault(properties, "quarkus.prop-doc.prop.file-name", "default.properties");
+        this.outputPath = properties.getProperty("quarkus.prop-doc.output-path", "").trim();
+        this.outputDescription = Boolean.parseBoolean(properties.getProperty("quarkus.prop-doc.output-description", "false"));
+    }
+
+    private String getPropertyOrDefault(Properties properties, String key, String defaultValue) {
+        return properties.getProperty(key, defaultValue).trim();
+    }
+
+    public List<String> getTemplatesToGenerate() {
+        return templatesToGenerate;
+    }
+
+    public String getTargetEnvironment() {
+        return targetEnvironment;
+    }
+
+    public String getCmFileName() {
+        return cmFileName;
+    }
+
+    public String getEnvFileName() {
+        return envFileName;
+    }
+
+    public String getPropFileName() {
+        return propFileName;
+    }
+
+    public String getOutputPath() {
+        return outputPath;
+    }
+
+    public boolean isOutputDescription() {
+        return outputDescription;
+    }
+}
