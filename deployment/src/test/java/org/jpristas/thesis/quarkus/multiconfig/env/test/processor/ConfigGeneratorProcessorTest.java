727d01f25b8a358fe4428b48f19bac47c7813d6f
Jakub Pristas <jakub.pristas@ibm.com>
2025-03-01 16:13:42 +0100

    added unit tests for reader and generator

--- deployment/src/test/java/org/jpristas/thesis/quarkus/multiconfig/env/test/processor/ConfigGeneratorProcessorTest.java
+++ deployment/src/test/java/org/jpristas/thesis/quarkus/multiconfig/env/test/processor/ConfigGeneratorProcessorTest.java
@@ -1,58 +1,113 @@
-//package org.jpristas.thesis.quarkus.multiconfig.env.test.processor;
-//
-//import io.quarkus.deployment.annotations.BuildProducer;
-//import io.quarkus.deployment.builditem.GeneratedResourceBuildItem;
-//import io.quarkus.deployment.pkg.builditem.OutputTargetBuildItem;
-//import io.quarkus.test.Mock;
-//import io.quarkus.test.InjectMock;
-//import io.quarkus.test.QuarkusUnitTest;
-//import org.jboss.shrinkwrap.api.ShrinkWrap;
-//import org.jboss.shrinkwrap.api.asset.EmptyAsset;
-//import org.jboss.shrinkwrap.api.spec.JavaArchive;
-//import org.jpristas.thesis.quarkus.multiconfig.env.deployment.builditem.ConfigDataBuildItem;
-//import org.jpristas.thesis.quarkus.multiconfig.env.deployment.processor.ConfigGeneratorProcessor;
-//import org.junit.jupiter.api.Test;
-//import org.junit.jupiter.api.extension.RegisterExtension;
-//import static org.mockito.Mockito.*;
-//
-//import java.io.IOException;
-//
-//public class ConfigGeneratorProcessorTest {
-//    @RegisterExtension
-//    static final QuarkusUnitTest config = new QuarkusUnitTest()
-//            .setArchiveProducer(() -> ShrinkWrap.create(JavaArchive.class)
-//                    .addClass(ConfigGeneratorProcessor.class)
-//                    .addAsManifestResource(EmptyAsset.INSTANCE, "beans.xml"));
-//
-////    @InjectMock
-////    ConfigGeneratorProcessor configGeneratorProcessor;
-//
-////    ConfigGeneratorProcessor configGeneratorProcessor = new ConfigGeneratorProcessor();
-//
-////    @Mock
-////    BuildProducer<GeneratedResourceBuildItem> resourceProducer;
-////    BuildProducer<GeneratedResourceBuildItem> resourceProducer = mock(BuildProducer.class);
-//
-////    @Mock
-////    OutputTargetBuildItem outputTarget;
-////    OutputTargetBuildItem outputTarget = mock(OutputTargetBuildItem.class);
-//
-//    @Test
-//    void testGenerateFiles() throws IOException {
-//        ConfigGeneratorProcessor configGeneratorProcessor = new ConfigGeneratorProcessor();
-//        OutputTargetBuildItem outputTarget = mock(OutputTargetBuildItem.class);
-//        BuildProducer<GeneratedResourceBuildItem> resourceProducer = mock(BuildProducer.class);
-//
-//        String mockConfig = "prop-doc.selected-templates=cm,env,prop\n" +
-//                "prop-doc.target_environment=prod\n" +
-//                "prop-doc.cm.file-name=config.cm\n" +
-//                "prop-doc.env.file-name=config.env\n" +
-//                "prop-doc.prop.file-name=config.properties\n";
-//
-//        ConfigDataBuildItem configData = new ConfigDataBuildItem(mockConfig);
-//
-//        configGeneratorProcessor.generateFiles(configData, outputTarget, resourceProducer);
-//
-//        verify(resourceProducer, atLeastOnce()).produce(any(GeneratedResourceBuildItem.class));
-//    }
-//}
+package org.jpristas.thesis.quarkus.multiconfig.env.test.processor;
+
+import io.quarkus.deployment.annotations.BuildProducer;
+import io.quarkus.deployment.builditem.GeneratedResourceBuildItem;
+import io.quarkus.deployment.pkg.builditem.OutputTargetBuildItem;
+import org.jpristas.thesis.quarkus.multiconfig.env.deployment.builditem.ConfigDataBuildItem;
+import org.jpristas.thesis.quarkus.multiconfig.env.deployment.processor.ConfigGeneratorProcessor;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.io.TempDir;
+import org.mockito.ArgumentCaptor;
+
+import java.io.IOException;
+import java.nio.file.Path;
+import java.util.Optional;
+import java.util.Properties;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.*;
+
+class ConfigGeneratorProcessorTest {
+
+    private ConfigGeneratorProcessor processor;
+    private BuildProducer<GeneratedResourceBuildItem> resourceProducer;
+    private OutputTargetBuildItem outputTarget;
+
+    @BeforeEach
+    void setUp(@TempDir Path tempDir) {
+        processor = new ConfigGeneratorProcessor();
+        resourceProducer = mock(BuildProducer.class);
+//        outputTarget = mock(OutputTargetBuildItem.class);
+
+        outputTarget = new OutputTargetBuildItem(
+                tempDir,            // Path outputDirectory
+                "test-base-name",   // baseName
+                "test-base-name",   // originalBaseName
+                false,              // rebuild
+                new Properties(),
+                Optional.empty()
+        );
+    }
+
+    @Test
+    void testNoContent_skipsGeneration() throws IOException {
+        // Given empty content
+        ConfigDataBuildItem configData = new ConfigDataBuildItem("");
+
+        // When
+        processor.generateFiles(configData, outputTarget, resourceProducer);
+
+        // Then: no interactions
+        verifyNoInteractions(resourceProducer);
+    }
+
+    @Test
+    void testGenerateAllTemplates() throws IOException {
+        // Given config data that selects cm, env, prop
+        String fileContent = """
+                prop-doc.selected-templates=cm,env,prop
+                prop-doc.target_environment=dev
+                prop-doc.cm.file-name=configmap.yaml
+                prop-doc.env.file-name=my.env
+                prop-doc.prop.file-name=application.properties
+                prop-doc.output-description=true
+                """;
+        ConfigDataBuildItem configData = new ConfigDataBuildItem(fileContent);
+
+        // When
+        processor.generateFiles(configData, outputTarget, resourceProducer);
+
+        // Then: we expect 3 calls to produce() (one for each template)
+        verify(resourceProducer, times(3)).produce(any(GeneratedResourceBuildItem.class));
+    }
+
+    @Test
+    void testGenerateSomeTemplates() throws IOException {
+        // Given only cm and prop are selected
+        String fileContent = """
+                prop-doc.selected-templates=cm,prop
+                prop-doc.target_environment=stage
+                """;
+        ConfigDataBuildItem configData = new ConfigDataBuildItem(fileContent);
+
+        // When
+        processor.generateFiles(configData, outputTarget, resourceProducer);
+
+        // Then: 2 calls (cm + prop)
+        verify(resourceProducer, times(2)).produce(any(GeneratedResourceBuildItem.class));
+    }
+
+    @Test
+    void testVerifyGeneratedResourceNames() throws IOException {
+        // Suppose we only generate cm to check the resource name
+        String fileContent = """
+                prop-doc.selected-templates=cm
+                prop-doc.cm.file-name=myconfigmap.yaml
+                """;
+        ConfigDataBuildItem configData = new ConfigDataBuildItem(fileContent);
+
+        processor.generateFiles(configData, outputTarget, resourceProducer);
+
+        // Capture the GeneratedResourceBuildItem argument to verify resource name
+        ArgumentCaptor<GeneratedResourceBuildItem> captor =
+                ArgumentCaptor.forClass(GeneratedResourceBuildItem.class);
+        verify(resourceProducer, times(1)).produce(captor.capture());
+
+        GeneratedResourceBuildItem producedResource = captor.getValue();
+        // If FileGenerationUtil sets the item name to match the fileName,
+        // we can check it here:
+        assertEquals("myconfigmap.yaml", producedResource.getName());
+    }
+}
--- deployment/src/test/java/org/jpristas/thesis/quarkus/multiconfig/env/test/processor/ConfigReaderProcessorTest.java
+++ deployment/src/test/java/org/jpristas/thesis/quarkus/multiconfig/env/test/processor/ConfigReaderProcessorTest.java
@@ -2,40 +2,37 @@ package org.jpristas.thesis.quarkus.multiconfig.env.test.processor;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-import io.quarkus.test.QuarkusUnitTest;
-import org.jboss.shrinkwrap.api.ShrinkWrap;
-import org.jboss.shrinkwrap.api.asset.EmptyAsset;
-import org.jboss.shrinkwrap.api.spec.JavaArchive;
 import org.jpristas.thesis.quarkus.multiconfig.env.deployment.builditem.ConfigDataBuildItem;
 import org.jpristas.thesis.quarkus.multiconfig.env.deployment.processor.ConfigReaderProcessor;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.RegisterExtension;
 
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 
 public class ConfigReaderProcessorTest {
-    @RegisterExtension
-    static final QuarkusUnitTest config = new QuarkusUnitTest()
-            .setArchiveProducer(() -> ShrinkWrap.create(JavaArchive.class)
-                    .addClass(ConfigReaderProcessor.class)
-                    .addAsManifestResource(EmptyAsset.INSTANCE, "beans.xml"));
 
     @Test
     void testConfigReaderProcessor() throws IOException {
-        String mockConfig = "prop-doc.selected-templates=cm,env\n" +
-                "prop-doc.target_environment=prod\n" +
-                "prop-doc.cm.file-name=config.cm\n" +
-                "prop-doc.env.file-name=config.env\n";
+        String mockConfig = """
+            prop-doc.selected-templates=cm,env
+            prop-doc.target_environment=prod
+            prop-doc.cm.file-name=config.cm
+            prop-doc.env.file-name=config.env
+            """;
 
+        // Create a temporary file with our mock config
         Path tempFile = Files.createTempFile("config", ".properties");
         Files.writeString(tempFile, mockConfig);
 
+        // Use a System property that ConfigReaderProcessor reads
         System.setProperty("example.config-reader.file.path", tempFile.toString());
 
+        // Instantiate the processor directly
         ConfigReaderProcessor processor = new ConfigReaderProcessor();
         ConfigDataBuildItem configDataBuildItem = processor.readApplicationProperties();
+
+        // Basic assertions
         assertNotNull(configDataBuildItem);
         assertEquals(mockConfig.trim(), configDataBuildItem.getFileContent().trim());
     }
